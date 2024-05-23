c5acb563a05d9eeaf494a7c67d367b970dc4991b
zhanabil <zhanabil.sydykov@bcc.kz>
2024-05-23 13:30:21 +0500

    add nav bar with items

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,22 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      bottomNavigationBar: CurvedNavigationBar(
+        color: Colors.blue.shade200,
+        backgroundColor: Colors.blue,
+        animationDuration: Duration(milliseconds: 300),
+        onTap: (index) {
+          print(index);
+        },
+        items: [
+          Icon(Icons.home, color: Colors.white),
+          Icon(Icons.favorite, color: Colors.white),
+          Icon(Icons.person, color: Colors.white),
+          Icon(Icons.settings, color: Colors.white),
+        ],
+      ),
+    );
   }
 }
